b339730ba9540113db8514946fe9e362488d37c7
Melor1an <san.boy@ro.ru>
2021-11-10 21:04:24 +0200

    Добавьте файлы проекта.

---
+++ WebApplication4/Data/AppDBInitializer.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication4.Models;
+
+namespace WebApplication4.Data
+{
+    public class AppDBInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+                if (!context.Books.Any())
+                {
+                    context.Books.AddRange(new Book()
+                    {
+                        Title = "1st book title",
+                        Description = "1st book desc",
+                        DateAdded = DateTime.Now
+                    },
+                    new Book()
+                    {
+                        Title = "2nd book title",
+                        Description = "2nd book desc",
+                        DateAdded = DateTime.Now
+                    });
+
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
